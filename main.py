dadfb0b51ba2307af5fb81e973f3823e13843f74
DhruvArora96 <da1414@ic.ac.uk>
2017-02-21 12:30:12 +0000

    Final_Version
    Update with finishing comments and temp threshold variables.

--- main.py
+++ main.py
@@ -5,94 +5,100 @@ import network
 import machine
 import ujson
 
-# construct an I2C bus
+#Constructing an I2C bus
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 
-#Defining broker address, ssid and broker password
+#Defining broker address, SSID and broker password
+
+#Details to connect to College broker
 #BROKER_ADDRESS= "192.168.0.10"   
-# SSID = 'EEERover'
-# PASSWORD = 'exhibition'
+#SSID = 'EEERover'
+#PASSWORD = 'exhibition'
+
+#Details to connect to Thingspeak broker
 BROKER_ADDRESS= "mqtt.thingspeak.com"
 SSID = 'Xperia Z3_62d1'
 PASSWORD = 'exhibition'
 
 #Connecting to the network
 sta_if = network.WLAN(network.STA_IF)
-
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
 sta_if.active(True)
 sta_if.connect(SSID, PASSWORD)
 
-#Wait for network to connect before proceeding with execution of code.
+#Waiting for network to connect before proceeding with execution of code.
 while not sta_if.isconnected():
   pass
 print("Connected to network")
-#Activating the real time clock and setting its first value.
+
+#Activating the real time clock and initialising its value.
 rtc = machine.RTC()
 rtc.datetime((2017, 2, 15, 10, 48, 0, 0, 0))
 
-#Defining thingspeak channel information and connect to it.
+#Defining thingspeak channel information and connecting to it.
 thingspeakChannelId = "227308" 
 thingspeakChannelWriteapi = "D14PP8N2KQE082IJ"
 c = MQTTClient("Dhruv",BROKER_ADDRESS, 1883)  
 c.connect()
 
+#Defining temperature threshold for abnormal temperature outside of cooking hours.
 temp_threshold= 32
+##Defining temperature threshold for oil fires.
 temp_burning= 280
 
-#Define a function to fill an array of 48 elements, each corresponding to half an hour slots, 
+#Defining a function to fill an array of 48 elements, each corresponding to half an hour slots, 
 #starting at midnight and ending at midnight of the next day.
-#If it is a time when you are supposed to be cooking, set the slot to 1. Otherwise set to 0.
+#Time slots in which the user usually cooks are set to 1. Otherwise set to 0.
 def set_array(array):
-	array[0] = 0
-	array[1] = 0
-	array[2] = 0
-	array[3] = 0
-	array[4] = 0
-	array[5] = 0
-	array[5] = 0
-	array[7] = 0
-	array[8] = 0
-	array[9] = 0
-	array[10] = 0
-	array[11] = 0
-	array[12] = 0
-	array[13] = 0
-	array[14] = 0
-	array[15] = 1
-	array[16] = 0
-	array[17] = 0
-	array[18] = 0
-	array[19] = 0
-	array[20] = 0
-	array[21] = 0
-	array[22] = 0
-	array[23] = 1
-	array[24] = 1
-	array[25] = 1
-	array[26] = 1
-	array[27] = 0
-	array[28] = 0
-	array[29] = 0
-	array[30] = 0
-	array[31] = 0
-	array[32] = 0
-	array[33] = 0
-	array[34] = 0
-	array[35] = 0
-	array[36] = 0
-	array[37] = 1
-	array[38] = 1
-	array[39] = 1
-	array[40] = 1
-	array[41] = 1
-	array[42] = 0
-	array[43] = 0
-	array[44] = 0
-	array[45] = 0
-	array[46] = 0
-	array[47] = 0
+	array[0] = 0 #00:00 am
+	array[1] = 0 #00:30 am
+	array[2] = 0 #01:00 am
+	array[3] = 0 #01:30 am
+	array[4] = 0 #02:00 am
+	array[5] = 0 #02:30 am
+	array[5] = 0 #03:00 am
+	array[7] = 0 #03:30 am
+	array[8] = 0 #04:00 am
+	array[9] = 0 #04:30 am
+	array[10] = 0 #05:00 am
+	array[11] = 0 #05:30 am
+	array[12] = 0 #06:00 am
+	array[13] = 0 #06:30 am
+	array[14] = 0 #07:00 am
+	array[15] = 1 #07:30 am
+	array[16] = 0 #08:00 am
+	array[17] = 0 #08:30 am
+	array[18] = 0 #09:00 am
+	array[19] = 0 #09:30 am
+	array[20] = 0 #10:00 am
+	array[21] = 0 #10:30 am
+	array[22] = 0 #11:00 am
+	array[23] = 1 #11:30 am
+	array[24] = 1 #12:00 am
+	array[25] = 1 #12:30 am
+	array[26] = 1 #01:00 pm
+	array[27] = 0 #01:30 pm
+	array[28] = 0 #02:00 pm
+	array[29] = 0 #02:30 pm
+	array[30] = 0 #03:00 pm
+	array[31] = 0 #03:30 pm
+	array[32] = 0 #04:00 pm
+	array[33] = 0 #04:30 pm
+	array[34] = 0 #05:00 pm
+	array[35] = 0 #05:30 pm
+	array[36] = 0 #06:00 pm
+	array[37] = 1 #06:30 pm
+	array[38] = 1 #07:00 pm
+	array[39] = 1 #07:30 pm
+	array[40] = 1 #08:00 pm
+	array[41] = 1 #08:30 pm
+	array[42] = 0 #09:00 pm
+	array[43] = 0 #09:30 pm
+	array[44] = 0 #10:00 pm
+	array[45] = 0 #10:30 pm
+	array[46] = 0 #11:00 pm
+	array[47] = 0 #11:30 pm
 
 #Declaring a an array of 48 bytearray and fill that array through the set_array function.
 time_array=bytearray(48)
@@ -117,6 +123,8 @@ while True:
 	
 	#Reading temperature data from the die register on the sensor.
 	raw1= i2c.readfrom_mem(64,0x01,2)
+	
+	#Converting the temperature data in Celsius.
 	cTemp1 = (raw1[0]*256 + raw1[1])/4
 	if cTemp1 > 8191 :
 		cTemp1 -= 16384
@@ -144,20 +152,24 @@ while True:
 	#Retrieving the value of the time_array index corresponding to the current 30 mins slot and storing it in a flag for cooking.
 	cooking_flag=time_array[thirty_min_segment]
 	
-	#If you are not supposed to be cooking but the temperature sensed is above a threshold, publish an alert.
+	#If the user is not expected to be cooking but the temperature sensed is above cooking threshold, publish an alert.
 	if cooking_flag==0 and cTemp>temp_threshold :
-		#c.publish(b'esys\FIDES\temp',str(output(now,cTemp
-		
+				
 		#Publishing to ThingSpeak.
 		credentials = "channels/{:s}/publish/{:s}".format(thingspeakChannelId, thingspeakChannelWriteapi)  
 		payload = "field1={:}&field3=1\n".format(output(now, cTemp))
 		c.publish(credentials, payload)
 		print(str(output(tm,cTemp)))
 		
-	#If you are supposed to be cooking but the temperature sensed is above a safe temperature, publish an alert.
+	#Regardless of whether the user is expected to be cooking, if the temperature sensed is above oil fire temperature, publish an alert.
 	elif cTemp > temp_burning :
-		dummyvariablet=1
-		
+			
+		#Publishing to ThingSpeak.
+		credentials = "channels/{:s}/publish/{:s}".format(thingspeakChannelId, thingspeakChannelWriteapi)  
+		payload = "field1={:}&field3=1\n".format(output(now, cTemp))
+		c.publish(credentials, payload)
+		print(str(output(tm,cTemp)))
+				
 	time.sleep(5)
 
 	
